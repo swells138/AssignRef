1b016eceb0149cc90089386cd00b3f301053bf1f
Sydney Wells <116097229+swells138@users.noreply.github.com>
2023-05-16 16:00:32 -0400

    Update BaseFile.cs

--- .net/Files/BaseFile.cs
+++ .net/Files/BaseFile.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sabio.Models.Domain.Files
+namespace AssignRef.Models.Domain.Files
 {
     public class BaseFile
     {
