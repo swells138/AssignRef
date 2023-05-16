ce675a9153bc618beae7a525b5140b864e271482
swells138 <sydneywells103@gmail.com>
2023-05-16 15:46:15 -0400

    dotnet added for files

---
+++ .net/Files/File.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Domain.Files
+{
+    public class File : BaseFile
+    {
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime DateCreated { get; set; }
+        public LookUp FileType { get; set; }
+
+    }
+}
---
+++ .net/Files/IFileService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Sabio.Models;
+using Sabio.Models.Domain.Files;
+using Sabio.Models.Requests.Files;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace Sabio.Services.Interfaces
+{
+    public interface IFileService
+    {
+        void DeleteById(int deleteId);
+        Paged<File> GetAll(int pageIndex, int pageSize, bool isDeleted);
+        Paged<File> GetByCreatedBy(int pageIndex, int pageSize, int id);
+        BaseFile Add(FileAddRequest model, int userId);
+        Paged<File> Search(int pageIndex, int pageSize, bool isDeleted, string query);
+        Task<string> UploadFile(IFormFile files);
+        int GetFileTypeByExt(string file);
+
+    }
+}
